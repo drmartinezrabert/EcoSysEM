16d025cc178edd6db965bd3fad1473aeecd386d2
soundslikealloy <eloi.mrp@gmail.com>
2024-09-12 18:20:13 +0200

    Update getRxnByComp()
    Now multiple compounds can be requested.

--- ecosysem/reactions.py
+++ ecosysem/reactions.py
@@ -37,39 +37,68 @@ class Reactions:
             Information of reaction if any (in parenthesis in csv/Excel file)
         
         """
-        if isinstance(compound, list):
-            if len(compound) > 1:
-                print('!EcoSysEM.Error: Only one compound can be requested.')
-                sys.exit()
-            else:
-                compound = compound[0]
-        if not isinstance(compound, str): compound = str(compound)
+    
+    def getRxnByComp(typeRxn, compounds):
+        """
+        Function to get reaction(s) involving the requested compound.
+    
+        Parameters
+        ----------
+        typeRxn : STR
+            What reaction(s) type are requested, matching with csv name. E.g.:
+                - 'pHSpeciation': pH equilibrium
+                - 'metabolisms': metabolic activities
+        compounds : STR or LIST
+            Name(s) of requested compound(s).
+            STR - one compound; LIST - multiple compounds.
+        
+        Returns
+        -------
+        rComp : LIST
+            Involving compounds of reaction(s).
+        mRxn : np.array
+            Reaction matrix (compounds)x(reactions).        
+        infoRxn : LIST
+            Information of reaction if any (in parenthesis in csv/Excel file)
+        
+        """
+        if not isinstance(typeRxn, str): typeRxn = str(typeRxn)
+        if not isinstance(compounds, list): compounds = [compounds]
         dRxn = pd.read_csv(Reactions.path + typeRxn + '.csv')
-        dRxnAux = dRxn
+        headers = np.empty(0)
         infoRxn = np.empty(0)
-        # for iCompound in compounds:
-        dRxnAux1 = dRxn.filter(regex = f'^{compound}/').dropna(how = 'all')
-        dRxnAux2 = dRxn.filter(regex = f'/{compound}/').dropna(how = 'all')
-        dRxnAux3 = dRxn.filter(like = f'/{compound} ').dropna(how = 'all')
-        if not dRxnAux1.empty:
-            dRxnAux = dRxnAux1
-        elif not dRxnAux2.empty:
-            dRxnAux = dRxnAux2
-        elif not dRxnAux3.empty:
-            dRxnAux = dRxnAux3
-        else:
+        for iCompound in compounds:
+            dRxnAux1 = dRxn.filter(regex = f'^{iCompound}/')
+            dRxnAux2 = dRxn.filter(regex = f'/{iCompound}/')
+            dRxnAux3 = dRxn.filter(like = f'/{iCompound} ')
+            if not dRxnAux1.empty:
+                dRxnAux = dRxnAux1
+            elif not dRxnAux2.empty:
+                dRxnAux = dRxnAux2
+            elif not dRxnAux3.empty:
+                dRxnAux = dRxnAux3
+            else:
+                dRxnAux = dRxnAux1
+            if dRxnAux.empty:
+                if typeRxn != 'pHSpeciation':
+                    print(f'!EcoSysEM.Warning: Reactions with {iCompound} not found.')
+            else:
+                iHeader = dRxnAux.columns.values[0:]
+                for iHead in iHeader:
+                    # Check if reaction has been already selected
+                    c_Head = np.where(headers == iHead)[0].size
+                    if c_Head == 0:
+                        iRxn = iHead[iHead.find('(')+1:iHead.find(')')]
+                        infoRxn = np.append(infoRxn, iRxn)
+                        headers = np.append(headers, iHead)
+        if infoRxn.size == 0:
             return None, None, None
-        dRxn = pd.concat([dRxn.iloc[dRxnAux.index, [0]], dRxnAux], axis=1).fillna(0)
-        rComp = list(dRxn['Compounds'])
-        mRxn = np.array(dRxn.loc[:, dRxn.columns != 'Compounds'])
-        headers = dRxn.columns.values[1:]
-        for iHead in headers:
-            if iHead.find('(') != -1:
-                newInfo = iHead[iHead.find('(')+1:iHead.find(')')]
-                infoRxn = np.append(infoRxn, newInfo)
-        if not infoRxn.any():
-            infoRxn = None
-        return rComp, mRxn, infoRxn
+        else:
+            dRxnF = dRxn[headers].dropna(how = 'all')
+            dRxnF = pd.concat([dRxn.iloc[dRxnF.index, [0]], dRxnF], axis=1).fillna(0)
+            rComp = list(dRxnF['Compounds'])
+            mRxn = np.array(dRxnF.loc[:, dRxnF.columns != 'Compounds'])
+            return rComp, mRxn, infoRxn
 
     def getRxnByName(typeRxn, nameRxn):
         """
@@ -123,6 +152,9 @@ class Reactions:
 #- Info of functions and examples -#
 ### Get reactions involving one or more compounds
 #> Reactions.getRxn(typeRxn, compounds), where compound can be a string (one compound) or list (multiple compounds)
+# print('')
+# rComp, mRxn, infoRxn = Reactions.getRxnByComp('metabolisms', ['NH3'])
+# print(rComp)
 # print(mRxn)
 # print(infoRxn)
 #----------------------------------#
