eabe185d9a45e7dc3c5107e35494f9353cd1a797
Eloi Martinez-Rabert <81569132+soundslikealloy@users.noreply.github.com>
2024-11-12 23:26:42 +0100

    Update getRxnByComp()
    Modification of header format of reaction database.

--- ecosysem/reactions.py
+++ ecosysem/reactions.py
@@ -141,7 +141,76 @@ class Reactions:
             rComp = list(dRxnF['Compounds'])
             mRxn = np.array(dRxnF.loc[:, dRxnF.columns != 'Compounds'])
             return rComp, mRxn, infoRxn
-
+        
+    def getRxnByComp(typeRxn, compounds, warnings = False):
+        """
+        Function to get reaction(s) involving the requested compound.
+    
+        Parameters
+        ----------
+        typeRxn : STR
+            What reaction(s) type are requested, matching with csv name. E.g.:
+                - 'pHSpeciation': pH equilibrium
+                - 'metabolisms': metabolic activities
+        compounds : STR or LIST
+            Name(s) of requested compound(s).
+            STR - one compound; LIST - multiple compounds.
+        warnings : BOOL
+            Display function warnings. Default: False.
+            
+        Returns
+        -------
+        rComp : LIST
+            Involving compounds of reaction(s).
+        mRxn : np.array
+            Reaction matrix (compounds)x(reactions).        
+        infoRxn : LIST
+            Information of reaction if any (in parenthesis in csv/Excel file)
+        
+        """
+        if not isinstance(typeRxn, list): 
+            if not isinstance(typeRxn, str): typeRxn = str(typeRxn)
+            typeRxn = [typeRxn]
+        if not isinstance(compounds, list): compounds = [compounds]
+        dRxn = pd.read_csv(Reactions.path + typeRxn[0] + '.csv')
+        rComp = list(dRxn['Compounds'])
+        diffComp = []
+        if len(typeRxn) > 1:
+            typeRxn = typeRxn[1:]
+            for iDB in typeRxn:
+                dRxn_aux = pd.read_csv(Reactions.path + iDB + '.csv')
+                rComp_aux = list(dRxn_aux['Compounds'])
+                diffComp_aux = list(set(rComp_aux)-set(rComp))
+                diffComp += diffComp_aux
+            dRxn = pd.concat([dRxn, pd.DataFrame({'Compounds': diffComp})], ignore_index=True)
+            for iDB in typeRxn:
+                dRxn_aux = pd.read_csv(Reactions.path + iDB + '.csv')
+                dRxn = pd.merge(dRxn, dRxn_aux, on='Compounds', how='left')
+        headers = np.empty(0)
+        infoRxn = np.empty(0)
+        for iCompound in compounds:
+            dRxnAux = dRxn[dRxn['Compounds'].isin([iCompound])].dropna(axis='columns')
+            if dRxnAux.empty:
+                if warnings:
+                    print(f'!EcoSysEM.Warning: Reactions with {iCompound} as a compound not found.')
+            else:
+                iHeader = dRxnAux.columns.values[1:]
+                for iHead in iHeader:
+                    # Check if reaction has been already selected
+                    c_Head = np.where(headers == iHead)[0].size
+                    if c_Head == 0:
+                        iRxn = iHead[iHead.find('(')+1:iHead.find(')')]
+                        infoRxn = np.append(infoRxn, iRxn)
+                        headers = np.append(headers, iHead)
+        if infoRxn.size == 0:
+            return None, None, None
+        else:
+            dRxnF = dRxn[headers].dropna(how = 'all')
+            dRxnF = pd.concat([dRxn.iloc[dRxnF.index, [0]], dRxnF], axis=1).fillna(0)
+            rComp = list(dRxnF['Compounds'])
+            mRxn = np.array(dRxnF.loc[:, dRxnF.columns != 'Compounds'])
+            return rComp, mRxn, infoRxn
+    
     def getRxnByName(typeRxn, nameRxn, warnings = False):
         """
         Function to get reaction(s) with the info/name (parenthesis in Excel).
