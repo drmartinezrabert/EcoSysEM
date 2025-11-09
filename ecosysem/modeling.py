2e99bbe4e326464de4feff261f304fb08eed3729
ZoeLem-pro <zoelemaire.pro@gmail.com>
2025-11-09 16:19:01 +0100

    Update modeling.py

--- ecosysem/modeling.py
+++ ecosysem/modeling.py
@@ -11,12 +11,15 @@ import numpy as np
 from scipy.integrate import ode
 import matplotlib.pyplot as plt
 import sys
+from time import process_time
 
 # Import classes from modules (as abb.) or import with importlib
 from envdef import ISA
 from reactions import KinRates as KR
 from thermodynamics import ThSA
 from bioenergetics import CSP
+
+import copy
 
 
 class MSMM:
@@ -46,6 +49,7 @@ class MSMM:
         _metaboProperties['moderate'] = {'protein turnover rate':5 ,'specific metabolic shift rates':0.2}
         _metaboProperties['slow'] = {'protein turnover rate':14 ,'specific metabolic shift rates':0.071}    
         dMtbRates = pd.DataFrame(data = _metaboProperties)
+        
         self.envModel = envModel
         self.atmModels = ['ISA', 'MERRA2', 'CAMS', 'ISAMERRA2', 'CAMSMERRA2']
         #!!! other models?
@@ -67,6 +71,11 @@ class MSMM:
         #self.time = time                #[setTime method] -> plot arg 
         #if plotMSMM == True:
         #self.plotMSMM(Bini, time) -> required args must be given when instance is created
+        #AJOUT ALEXIS
+        self._ODE_template = ode(f = self._ODEsystem_MSMM)
+        self._ODE_template.set_integrator('vode', method='adams')
+        
+
     
     def _callEnvP(self, envModel, envArgs):
         """
@@ -110,20 +119,20 @@ class MSMM:
                 else:
                     print('error in envArgs type')
                     sys.exit()
-                self.pH = ISAinst.pH.copy()
-                self.compositions = ISAinst.compositions.copy()
-                self.compounds = ISAinst.compounds.copy()
-                self.resolution = ISAinst.resolution.copy()
-                self.temperature = ISAinst.temperature.copy()
-                self.pressure = ISAinst.pressure.copy()
-                self.altitude = ISAinst.altitude.copy()
-                self.H2O = ISAinst.H2O.copy()
-                self.Pi = ISAinst.Pi.copy()           #!!! could be useful for MSMMv2
-                self.Ci_G = ISAinst.Ci_G.copy()        #!!! //
+                self.pH = ISAinst.pH
+                self.compositions = ISAinst.compositions
+                self.compounds = ISAinst.compounds
+                self.resolution = ISAinst.resolution
+                self.temperature = ISAinst.temperature
+                self.pressure = ISAinst.pressure
+                self.altitude = ISAinst.altitude
+                self.H2O = ISAinst.H2O
+                self.Pi = ISAinst.Pi        #!!! could be useful for MSMMv2
+                self.Ci_G = ISAinst.Ci_G       #!!! //
                 if self.Wtype == 'L_FW':
-                    self.Ct = ISAinst.Ci_LFW.copy()
+                    self.Ct = ISAinst.Ci_LFW
                 elif self.Wtype == 'L_SW':
-                    self.Ct = ISAinst.Ci_LSW.copy()
+                    self.Ct = ISAinst.Ci_LSW
                 else: print('Liquid phase could not be recognized. Enter "L_FW" or "L_SW".')
                 self.salinity = 0.0 #!!! for models other than atm, can be !=0
                 self.typeKin = 'MM-Arrhenius'
@@ -138,7 +147,7 @@ class MSMM:
             print('arg.error: environment model not found, see envModels')
             sys.exit()     
 
-    def _ODEsystem_MSMM(self, y, t, idP):
+    def _ODEsystem_MSMM(self, t, y, idP):
         """
         Function for the differential equations system of the model.
         
@@ -150,7 +159,7 @@ class MSMM:
         t : LIST or np.array
             Time range over which biomass variation is computed
         idP : INT
-            Parameter index from the plot function (involved parameters depends on envModel)
+            Parameter index from the plot function (involved parameters depend on envModel)
             
         Returns
         -------
@@ -165,18 +174,18 @@ class MSMM:
         Btot = sum(Blist)
         
         #import self.attributes
-        mortality = self.mortality.copy()
-        K = self.K.copy()
-        typeKin = self.typeKin.copy()
+        mortality = self.mortality  #??? no copy required for immutable data types like floats or strings
+        K = self.K
+        typeKin = self.typeKin
         paramDB = self.db.copy()
-        typeRxn = self.typeMtb.copy()
-        rxn = self.metabolism.copy()
-        specComp = self.eD.copy()
+        typeRxn = self.typeMtb
+        rxn = self.metabolism
+        specComp = self.eD
         T = self.temperature.copy()
-        pH = self.pH.copy()
-        S = self.salinity.copy()
+        pH = self.pH
+        S = self.salinity
         C = self.Ct.copy()
-        fluidType = self.fluidType.copy()
+        fluidType = self.fluidType
         # Set requested arguments for ThSA.getDeltaGr & KR.getRs
         if self.envModel in self.atmModels:
             DGr_args = {'typeRxn' : typeRxn,
@@ -251,12 +260,16 @@ class MSMM:
         Bm = Blist[1]
         Bs = Blist[2]
         
+        thetaGM = MSMM._stShifts(self, shift = 'GxM')
+        thetaMS = MSMM._stShifts(self, shift = 'MxS')
+        thetaSRIP = MSMM._stShifts(self, shift = 'S-RIP')
+        
         eta = self._specMtbShiftRates[self.mtbRates] 
-        Rm_g = Bm * eta * MSMM._stShifts(self, shift = 'GxM')[idP]
-        Rg_m = Bg * eta * (1 - MSMM._stShifts(self, shift = 'GxM')[idP])
-        Rs_m = Bs * eta * MSMM._stShifts(self, shift = 'MxS')[idP]
-        Rm_s = Bm * eta * (1 - MSMM._stShifts(self, shift = 'MxS')[idP])
-        Rs_rip = Bs * eta * (1 - MSMM._stShifts(self, shift = 'S-RIP')[idP])
+        Rm_g = Bm * eta * thetaGM[idP]
+        Rg_m = Bg * eta * (1 - thetaGM[idP])
+        Rs_m = Bs * eta * thetaMS[idP]
+        Rm_s = Bm * eta * (1 - thetaMS[idP])
+        Rs_rip = Bs * eta * (1 - thetaSRIP[idP])
         Rlist = [Rm_g, Rg_m, Rs_m, Rm_s, Rs_rip]
         return Rlist    #??? change return format 
         
@@ -280,19 +293,19 @@ class MSMM:
         # Set requested arguments for CSP.getAllCSP
         if self.envModel in self.atmModels:
             paramDB = self.db.copy()
-            typeKin = self.typeKin.copy()
-            typeMetabo = self.typeMtb.copy()
-            rxn = self.metabolism.copy()
-            specComp = self.eD.copy()
+            typeKin = self.typeKin      # no copy required for immutable data types like floats or strings
+            typeMetabo = self.typeMtb
+            rxn = self.metabolism
+            specComp = self.eD
             C = self.Ct.copy()
             T = self.temperature.copy()
-            pH = self.pH.copy()
-            S = self.salinity.copy()
-            fluidType = self.fluidType.copy()
-            DGsynth = self.DGsynth.copy()
-        else: print('error in CSPargs'), sys.exit() #!!!
+            pH = self.pH
+            S = self.salinity
+            fluidType = self.fluidType
+            DGsynth = self.DGsynth
+        else: print('error in CSPargs'), sys.exit() #!!! set getAllCSP args for other models
         
-        CSPargs = {'paramDB': paramDB,
+        CSPargs = {'paramDB': paramDB, 
                    'typeKin': typeKin,
                    'typeMetabo': typeMetabo,
                    'reaction': rxn,
@@ -309,13 +322,14 @@ class MSMM:
                    'solvent': 'H2O',
                    'asm': 'stoich',
                    'DGsynth': DGsynth}
-        #!!! set getAllCSP args for other models
-        st = self.st.copy()
+        
+        st = self.st
+        #Compute cell specific powers
         Pcat = CSP.getAllCSP(**CSPargs)['Pcat']
         Pm = CSP.getAllCSP(**CSPargs)['Pm0']
         Ps = CSP.getAllCSP(**CSPargs)['Ps']
         Pcell = CSP.getAllCSP(**CSPargs)['Pcell']
-        
+        #Compute shift controls (theta)
         if shift == 'GxM':
             theta = 1 / (np.exp((-Pcat + Pcell)/(st * Pcell)) +1)
         elif shift == 'MxS':
@@ -325,7 +339,10 @@ class MSMM:
         else: print('error in itheta value'), sys.exit() #!!!
         return theta
 
-    def solveODE(self, time, Bini, exportBint = False):
+    def solveODE(self, Bini, tSpan, dt = 1, exportBint = False):
+        print("debut")
+        print(type(dt))
+        print(tSpan)
         """ #!!! tooltip
         Function to plot solutions of the MSMM ODE system.
         
@@ -337,7 +354,7 @@ class MSMM:
             Time range over which the microbial dynamic is computed
         exportBint : BOOL
             Command to export integrated biomass values as Excel document.
-            Default is False.
+            Default is False. #!!! add export code
         
         Returns
         -------
@@ -345,8 +362,7 @@ class MSMM:
         [...]
         
         """
-        #ODEsol = ode(self._ODEsystem_MSMM)
-
+        if not isinstance(Bini, np.ndarray): Bini = np.array(Bini)
         if len(Bini) != 4:
             print('error in initial biomass, Bini must contain 4 elements') #!!!
             sys.exit()
@@ -355,20 +371,47 @@ class MSMM:
             alt = self.altitude.copy()
             isolve = len(alt)
         else: print('envModel not found') #!!! set needed variables for other models
-        if not isinstance(time, np.ndarray): time = np.array(time)
-        # Initialize Bint matrix
+        if not isinstance(tSpan, int): tSpan = int(tSpan)
+        #Initialize Bint matrix
         Bint = np.empty(Bini.shape)
         Bint = Bint[..., np.newaxis]
-        Bint = np.repeat(Bint, len(time), axis = -1)
+        Bint = np.repeat(Bint, tSpan+1, axis = -1)
         Bint = Bint[..., np.newaxis]
         Bint = np.repeat(Bint, isolve, axis = -1)
-
-        for i in range(isolve):
-            ode.set_f_params(i)
-            Bint[..., i] = ode.integrate(time) #step = True ?
+        #print('sol matrix shape:', Bint.shape)
+        print("test")
+        t0 = process_time()
+        ODEsol = copy.copy(self._ODE_template)
+        print(process_time() - t0)
+        
+        
+        
+        print("debut boucle for")
+        t0 = process_time()
+        for idP in range(isolve):
+             ODEsol.set_f_params(idP)
+             ODEsol.set_initial_value(Bini, 0)
+             #print(vars(ODEsol))
+             print(f'alt = {alt[idP]} m') # for envModel from atmModels only
+             print("debut boucle while")
+             while ODEsol.successful() and ODEsol.t < tSpan:
+                 print("while...")
+                 t0 = process_time()
+                 print(ODEsol.t+dt)
+                 sol = ODEsol.integrate(ODEsol.t+dt)
+                 print(sol)
+                 print(process_time() - t0)
+                 #int_status = ODEsol.get_return_code()
+                 #if int_status > 0: print('integration success')
+                 #elif int_status < 0: print('integration stopped early or failed')
+                 time = int(ODEsol.t)
+                 #print(time, sol)
+                 Bint[:,time,idP] = sol
+        print("fin boucle for")
+        print(process_time() - t0)
         return Bint
         
-    def plotMSMM(self, time, Bini):
+    def plotMSMM(self, Bini, idP, time, dt = 1):
         
         """ #!!! tooltip
         Function to plot solutions of the MSMM ODE system.
@@ -381,24 +424,26 @@ class MSMM:
         -------
         None (microbial dynamic is plotted, one microbial community at a time)
         """
-        # call ODEsolve fct
-        # if self.envModel in self.atmModels:
-        #     datmMicr = {'CH4': 'Methanotrophs',
-        #                 'H2': 'Hydrogen-oxidizing bacteria',
-        #                 'CO': 'CO-oxidizing bacteria'}
-        #     communityName = datmMicr[self.eD]
-            # plt.plot(time, Bplot[i][:,0],'g-', linewidth=2.0)    #growth state curve
-            # plt.plot(time, Bplot[i][:,1],'k-', linewidth=2.0)    #maintenance state curve
-            # plt.plot(time, Bplot[i][:,2],'b-', linewidth=2.0)    #survival state curve
-            # plt.plot(time, Bplot[i][:,3],'r--', linewidth=2.0)   #death state curve
-            # if self.envModel in self.atmModels:
-            #     if self.envModel == 'ISA':
-            #         plt.xlabel('time (hours)')
-            #         plt.ylabel('Cell concentration (cell/m^3 air)')
-            #         plt.title(f'Dynamic of the {communityName} community at {(alt[i]/1000)} km altitude')
-            # else: sys.exit() #!!! labels for other models 
-            # plt.legend(['Growth', 'Maintenance', 'Survival', 'Dead cells'], bbox_to_anchor = (1.4, 1.0), borderaxespad = 1, title = 'Metabolic states:', title_fontproperties = {'size': 'large', 'weight': 'bold'})
-            # plt.grid() 
-            # plt.show()
+        # call ODE solving function
+        Bplot = self.solveODE(Bini, time, dt)
 
+        if self.envModel in self.atmModels:
+            datmMicr = {'CH4': 'Methanotrophs',
+                        'H2': 'Hydrogen-oxidizing bacteria',
+                        'CO': 'CO-oxidizing bacteria'}
+            communityName = datmMicr[self.eD]
+            plt.plot(time, Bplot[0,:,idP],'g-', linewidth=2.0)    #growth state curve
+            plt.plot(time, Bplot[1,:,idP],'k-', linewidth=2.0)    #maintenance state curve
+            plt.plot(time, Bplot[2,:,idP],'b-', linewidth=2.0)    #survival state curve
+            plt.plot(time, Bplot[3,:,idP],'r--', linewidth=2.0)   #death state curve
+            if self.envModel in self.atmModels:
+                if self.envModel == 'ISA':
+                    plt.xlabel('time (hours)')
+                    plt.ylabel('Cell concentration (cell/m^3 air)')
+                    plt.title(f'Dynamic of the {communityName} community at {idP} km altitude')
+            else: sys.exit() #!!! labels for other models 
+            plt.legend(['Growth', 'Maintenance', 'Survival', 'Dead cells'], bbox_to_anchor = (1.4, 1.0), borderaxespad = 1, title = 'Metabolic states:', title_fontproperties = {'size': 'large', 'weight': 'bold'})
+            plt.grid() 
+            plt.show()
+        return
 
