909570aac1c2efd60b2b817f88be1d228eceeea4
Eloi Martinez-Rabert <81569132+drmartinezrabert@users.noreply.github.com>
2026-02-03 06:13:42 +0100

    Bug in `ecosysem_cmd.py` (getDataCAMS function)
    Now argument `_d` accepts string ('All'), integers and list of integers.

--- ecosysem/ecosysem_cmd.py
+++ ecosysem/ecosysem_cmd.py
@@ -69,7 +69,7 @@ elif function == 'getDataCAMS':
                         help="[int or list of int] Year(s) of requested data.")
     parser.add_argument('_m', nargs = '+', type = int,
                         help="[int or list of int] Month(s) of requested data.")
-    parser.add_argument('_d', default='All', nargs = '+', type = int,
+    parser.add_argument('_d', default='All', nargs = '+',
                         help="[int or list of int or str ('All'))] (Default: 'All') Day(s) of month of requested data. With 'All' get the whole month.")
     parser.add_argument('_hr', nargs = '+', default = [0, 12],
                         help="[int or list of int] (Default: '0 12') Hour(s) of requested data.")
@@ -86,10 +86,14 @@ elif function == 'getDataCAMS':
     # Argument definition
     args = parser.parse_args()
     dataType = args.dataType
-    if not np.all(np.isin(dataType, ['dly', 'mly'])): raise ValueError(f"Unaccepted dataType ({dataType}). Accepted dataType: 'dly', 'mly', 'cmly', 'All'.")
+    if not np.all(np.isin(dataType, ['dly', 'mly'])): raise ValueError(f"Unaccepted dataType ({dataType}). Accepted dataType: 'dly', 'mly'.")
     years = args.y; years.sort()
     months = args.m; months.sort()
     days = args.d
+    if not np.any(np.isin(days, 'All')):
+        days = sorted([int(d) for d in days])
+    else:
+        days = 'All'
     hours = args.hr
     dataset = args.dset
     pressure_levels = args.pressure; pressure_levels.sort()
